0ff11c3e56c5519a4387a374c821f1ef1e9b4c25
aymenBox <boxaymen7@gmail.com>
2022-08-19 16:28:10 +0100

    ading pagination logic and scrapping names

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
+import pandas as pd
 
+#create a dataframe with name specialite tel tel1 adresse 
+data = pd.DataFrame(columns=['name','specialite','tel','tel1','adresse'])
 driver = webdriver.Chrome()
 driver.get("https://tunisie-medicale.com/index.php/docteur")
 time.sleep(5)
@@ -9,5 +13,26 @@ page=0
 while(page<3000):
     page=page+20
     driver.get("https://tunisie-medicale.com/index.php/docteur/index/"+str(page))
-    time.sleep(5)
-driver.close()
+    #find divs with class="col-lg-6 col-md-6 col-sm-6 col-xs-12"
+    divs = driver.find_elements(By.CLASS_NAME,"col-lg-6.col-md-6.col-sm-6.col-xs-12")
+    print(divs)
+    links=[]
+    #get first links of each div
+    for div in divs:
+        links.append(div.find_element(By.TAG_NAME,"a").get_attribute("href"))
+    time.sleep(2)
+    for link in links:
+        #got to link
+        driver.get(link)
+        time.sleep(5)
+        #get element by itemprop="name"
+        name = driver.find_element(By.XPATH,"//*[@itemprop='name']")
+        print(name.text)
+        time.sleep(2)
+
+
+
+
+#save data to excel
+data.to_excel("docteur.xlsx")
+driver.quit()
